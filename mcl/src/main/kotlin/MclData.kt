76ff0086c3456b44061204b209e8844a9b397a7a
Voltual1 <18206039173@163.com>
2026-06-13 16:45:05 +0800

    update

--- mcl/src/main/kotlin/MclData.kt
+++ mcl/src/main/kotlin/MclData.kt
@@ -17,10 +17,12 @@ data class MclNode(
      * Minetest param1: (night_light << 4) | day_light
      */
     fun setLight(blockLight: Byte, skyLight: Byte) {
-        val dayLight = max(blockLight.toInt(), skyLight.toInt()) & 0x0F;
-        val nightLight = blockLight.toInt() & 0x0F;
-        this.param1 = ((nightLight shl 4) or dayLight).toByte()
-    }
+    // 将 & 替换为 and
+    val dayLight = (max(blockLight.toInt(), skyLight.toInt()) and 0x0F)
+    val nightLight = (blockLight.toInt() and 0x0F)
+    
+    this.param1 = ((nightLight shl 4) or dayLight).toByte()
+}
 }
 
 /**
