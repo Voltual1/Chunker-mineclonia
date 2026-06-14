e18ebc5a86b1b1621670d6c16fc177ea56014008
Voltual <3196570237@qq.com>
2026-06-14 17:47:58 +0800

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -27,7 +27,7 @@ android {
 
     defaultConfig {
         applicationId = "me.voltual.vb"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
