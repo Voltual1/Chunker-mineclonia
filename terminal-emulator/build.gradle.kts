330e88345d03e1b37c09bff3215c50d89746bca3
Voltual1 <18206039173@163.com>
2026-06-14 11:16:04 +0800

    update

--- terminal-emulator/build.gradle.kts
+++ terminal-emulator/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt")
+                getDefaultProguardFile("proguard-android-optimize.txt"),                
             )
         }
     }
