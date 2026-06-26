5dc9e9aef88d96259ca73071bf35ed8555706521
Voltual1 <18206039173@163.com>
2026-06-26 23:28:27 +0800

    Fix MclColumnWriter writeColumn return type mismatch with Java ColumnWriter interface

--- mcl/src/commonMain/kotlin/MclColumnWriter.kt
+++ mcl/src/commonMain/kotlin/MclColumnWriter.kt
@@ -2,10 +2,15 @@ package me.voltual.mcl
 
 import com.hivemc.chunker.conversion.encoding.base.writer.ColumnWriter
 import com.hivemc.chunker.conversion.intermediate.column.ChunkerColumn
+import com.hivemc.chunker.scheduling.task.Task
+import com.hivemc.chunker.scheduling.task.TaskWeight
+import java.lang.Void
 
 class MclColumnWriter(val manager: MclConverterManager) : ColumnWriter {
-    override fun writeColumn(chunkerColumn: ChunkerColumn) {
-        manager.convertColumn(chunkerColumn)
+    override fun writeColumn(chunkerColumn: ChunkerColumn): Task<Void> {
+        return Task.async("Mcl Write Column", TaskWeight.LOW) {
+            manager.convertColumn(chunkerColumn)
+        }
     }
 
     override fun flushColumns() {
