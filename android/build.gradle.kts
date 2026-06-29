fb25279bc5f7c4823bce179866722745a3da9f48
Voltual1 <18206039173@163.com>
2026-06-29 12:32:39 +0800

    3.1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "me.voltual.vb"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "3.0"
+        versionCode = 5
+        versionName = "3.1"
         multiDexEnabled = true
         buildConfigField("String", "LICENSE", "\"AGPLv3\"")
     }
