83882d7b7bc9a9e4b04e6e477aea0eab529f1c90
Voltual1 <18206039173@163.com>
2026-06-30 12:21:21 +0800

    build: 替换子模块的 fastutil 裁剪版为官方完整版，解决 Android 重复类冲突

--- converter/build.gradle.kts
+++ converter/build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("commons-io:commons-io:2.11.0")
     implementation("com.twelvemonkeys.imageio:imageio-tga:3.9.4")
-    implementation("com.nukkitx.fastutil:fastutil-int-object-maps:8.5.3")
+    implementation("it.unimi.dsi:fastutil:8.5.18")
     api("net.kyori:adventure-api:4.14.0")
     api("net.kyori:adventure-text-serializer-gson:4.14.0")
     api("net.kyori:adventure-text-serializer-legacy:4.14.0")
