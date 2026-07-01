5a4975e432660a62936093d4e7fdfca6ceab4c1b
Voltual1 <18206039173@163.com>
2026-07-01 20:00:23 +0800

    调整creative-api的build.gradle.kts依赖

--- creative-api/build.gradle.kts
+++ creative-api/build.gradle.kts
@@ -8,7 +8,8 @@ description = "A resource-pack library for Minecraft: Java Edition."
 
 dependencies {
     compileOnlyApi("org.jetbrains:annotations:26.0.2")
-    api("net.kyori:adventure-key:4.9.3")
-    api("net.kyori:adventure-api:4.14.0")
-    implementation("net.kyori:adventure-text-serializer-legacy:4.14.0")
+    api("net.kyori:adventure-key:4.24.0")
+    api("net.kyori:adventure-api:4.24.0")
+    api("com.google.code.gson:gson:2.13.1")
+    runtimeOnly("net.kyori:adventure-text-serializer-legacy:4.24.0")
 }
