0fa9d7b15b5b05a73a7a001b81dfbc183d73cd79
Voltual1 <18206039173@163.com>
2026-07-08 23:22:15 +0800

    ./gradlew assembleRelease

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "me.voltual.vb"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "6.0"
+        versionCode = 10
+        versionName = "7.0"
         multiDexEnabled = true
         buildConfigField("String", "LICENSE", "\"AGPLv3\"")
     }
