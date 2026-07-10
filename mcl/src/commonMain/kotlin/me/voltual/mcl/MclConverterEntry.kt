d340b27e32441f0e34649c90e86df590c5314d31
Voltual1 <18206039173@163.com>
2026-07-10 21:35:17 +0800

    Fix: update MclConverterEntry to support new MclConverterManager constructor signature

--- mcl/src/commonMain/kotlin/me/voltual/mcl/MclConverterEntry.kt
+++ mcl/src/commonMain/kotlin/me/voltual/mcl/MclConverterEntry.kt
@@ -1,3 +1,11 @@
+//Copyright (C) 2025 Voltual
+// 本程序是自由软件：你可以根据自由软件基金会发布的 GNU 通用公共许可证第3版
+//（或任意更新的版本）的条款重新分发和/或修改它。
+//本程序是基于希望它有用而分发的，但没有任何担保；甚至没有适销性或特定用途适用性的隐含担保。
+// 有关更多细节，请参阅 GNU 通用公共许可证。
+//
+// 你应该已经收到了一份 GNU 通用公共许可证的副本
+// 如果没有，请查阅 <http://www.gnu.org/licenses/>。
 package me.voltual.mcl
 
 import me.voltual.mcl.core.MclConverterManager
@@ -7,8 +15,18 @@ import com.hivemc.chunker.conversion.intermediate.column.ChunkerColumn
 import java.io.File
 
 object MclConverterEntry {
-    fun runConversion(columns: Iterable<ChunkerColumn>, outputFolder: String) {
-        val manager = MclConverterManager(File(outputFolder))
+    /**
+     * 运行转换任务，增加了对出生点坐标的支持
+     */
+    fun runConversion(
+        columns: Iterable<ChunkerColumn>, 
+        outputFolder: String,
+        spawnX: Int = 0,
+        spawnY: Int = 64,
+        spawnZ: Int = 0
+    ) {
+        // 传入 4 个参数以匹配 MclConverterManager 的新构造函数
+        val manager = MclConverterManager(File(outputFolder), spawnX, spawnY, spawnZ)
         
         var count = 0
         for (column in columns) {
